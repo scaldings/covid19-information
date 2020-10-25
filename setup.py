886b932f5acb6d967bb08fc70520923026799056
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-25 23:08:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='covid19-news',
+    name='covid19-information',
     version='1.0',
     packages=[''],
-    url='https://github.com/scaldings/covid19-information.git',
+    url='https://github.com/scaldings/covid19-information',
     license='',
     author='scaldings',
     author_email='',
-    description=''
+    description='A python script that provides information about the Coronavirus disease in a country'
 )
